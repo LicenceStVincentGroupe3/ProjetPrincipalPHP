f0b21f694c591e0acc5a81a6f301b40717acb8f7
Dylan Noally <dylan.window@hotmail.fr>
2018-11-27 17:09:34 +0100

    Clés étrangères

--- src/Entity/CompanyNbEmployee.php
+++ src/Entity/CompanyNbEmployee.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +23,16 @@ class CompanyNbEmployee
      */
     private $nbEmployee;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Company", mappedBy="idCompanyNbEmployee")
+     */
+    private $companies;
+
+    public function __construct()
+    {
+        $this->companies = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +46,37 @@ class CompanyNbEmployee
     public function setNbEmployee(int $nbEmployee): self
     {
         $this->nbEmployee = $nbEmployee;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Company[]
+     */
+    public function getCompanies(): Collection
+    {
+        return $this->companies;
+    }
+
+    public function addCompany(Company $company): self
+    {
+        if (!$this->companies->contains($company)) {
+            $this->companies[] = $company;
+            $company->setIdCompanyNbEmployee($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCompany(Company $company): self
+    {
+        if ($this->companies->contains($company)) {
+            $this->companies->removeElement($company);
+            // set the owning side to null (unless already changed)
+            if ($company->getIdCompanyNbEmployee() === $this) {
+                $company->setIdCompanyNbEmployee(null);
+            }
+        }
 
         return $this;
     }
