7f0f75d4c1f62173f3fdf2aae8c83e13749260f6
DylanP60 <popyk.dylan@9online.fr>
2019-01-07 12:13:15 +0100

    Modif Api

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,12 +11,12 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\VarDumper\VarDumper;
 
 /**
- * @Route("/contact", name="contact")
+ * @Route("/contact")
  */
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/new", name="new")
+     * @Route("/new", methods={"GET", "POST"})
      */
     public function new(Request $request)
     {
@@ -30,7 +30,6 @@ class ContactController extends AbstractController
     	if($form->isSubmitted() && $form->isValid())
         {
     	   $contact=$form->getData();
-           VarDumper::dump($contact);
 
            $entityManager = $this->getDoctrine()->getManager();
            // Persist prépare l'entité "contact" pour la création //
@@ -45,8 +44,37 @@ class ContactController extends AbstractController
         return $this->render('contact/new.html.twig', array('form' => $form->createView(),));
     }
 
+     /**
+     * @Route("/", methods={"POST"})
+     */
+    public function newApi(Request $request)
+    {
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        // La méthode handleRequest de la class form permet de récupérer les valeurs des champs dans les imputs du formulaire //
+        $form->handleRequest($request);
+    
+        if($form->isSubmitted() && $form->isValid())
+        {
+           $contact=$form->getData();
+
+           $entityManager = $this->getDoctrine()->getManager();
+           // Persist prépare l'entité "contact" pour la création //
+           $entityManager->persist($contact);
+           // Flush envoie les infos en base (ajout) //
+           $entityManager->flush();
+
+           return $this->redirect($this->generateUrl('listCont'));
+           //return $this->render('contact/new.html.twig', array('form' => $form->createView(),));
+        }   
+
+        return $this->render('contact/new.html.twig', array('form' => $form->createView(),));
+    }
+
     /**
-     * @Route("/edit/{id}", name="edit")
+     * @Route("/edit/{id}", methods={"GET", "POST"}, name="edit", requirements={"id"="\d+"})
      */
     public function edit($id, Request $request)
     {
@@ -67,7 +95,7 @@ class ContactController extends AbstractController
             $entityManager->flush();
 
             //VarDumper::dump($this->generateUrl('listCont'));die;
-            return $this->redirect($this->generateUrl('listCont'));
+            return $this->redirectToRoute('listCont');
 
         }
 
@@ -76,7 +104,37 @@ class ContactController extends AbstractController
     } 
 
     /**
-     * @Route("/delete/{id}", name="delete", requirements={"id"="\d+"})
+     * @Route("/", methods={"POST"}, name="edit", requirements={"id"="\d+"})
+     */
+    public function editApi($id, Request $request)
+    {
+        $repository = $this->getDoctrine()->getManager()->getRepository(Contact::class);
+
+        $editContact = $repository->find($id);
+
+        // Equivalent du SELECT * where id=(paramètre) //
+        $form = $this->createForm(ContactType::class, $editContact);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid() && $request->isMethod('POST'))
+        {
+            $editContact = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $editContact->setContactDateUpdatePlug(new \DateTime());
+            $entityManager->flush();
+
+            //VarDumper::dump($this->generateUrl('listCont'));die;
+            return $this->redirectToRoute('listCont');
+
+        }
+
+
+        return $this->render('contact/edit.html.twig', ['editContact' => $editContact, 'form' => $form->createView()]);
+    } 
+
+    /**
+     * @Route("/delete/{id}", methods={"GET", "POST"}, requirements={"id"="\d+"})
      */
     public function delete($id)
     {
@@ -90,6 +148,22 @@ class ContactController extends AbstractController
 
         return $this->redirectToRoute('listCont');
     }
+
+    /**
+     * @Route("/", methods={"POST"}, requirements={"id"="\d+"})
+     */
+    public function deleteApi($id)
+    {
+        $suppBD = $this->getDoctrine()->getManager();
+        // On créer un objet instance de contact
+        $suppContact = $suppBD->getRepository(Contact::class)->find($id);
+        // Suppression du contact
+        $suppBD->remove($suppContact);
+        // Execution
+        $suppBD->flush();
+
+        return $this->redirectToRoute('listCont');
+    }    
 
     /**
      * @Route("/list", name="listCont")
