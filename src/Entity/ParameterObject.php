79abe40899262c53afa18a37fb9a7e2323385332
DylanP60 <popyk.dylan@9online.fr>
2019-01-09 14:24:12 +0100

    Rectification asserts

--- src/Entity/ParameterObject.php
+++ src/Entity/ParameterObject.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ParameterObjectRepository")
@@ -20,6 +22,7 @@ class ParameterObject
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $parameterObjectObject;
 
