703f8855e1091476cf74ac2bb574b8f41ba73923
DylanP60 <popyk.dylan@9online.fr>
2019-04-23 14:06:32 +0200

    Design Contact et Form Opération

--- src/AdminBundle/Form/ContactType.php
+++ src/AdminBundle/Form/ContactType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -36,7 +36,7 @@ class ContactType extends AbstractType
 
     		->add('contactGender', ChoiceType::class, array('choices' => array('Homme'=>0, 'Femme'=>1)))
 
-    		->add('contactBirthDate', DateTimeType::class,  array('label' => 'Date de naissance'))
+    		->add('contactBirthDate', DateType::class,  array('label' => 'Date de naissance'))
     		->add('contactEmail', TextType::class, array('label' => 'Email'))
     		->add('contactMobilePhone', TextType::class, array('label' => 'Tel. Mobile'))
     		->add('contactDirectLandline', TextType::class, array('label' => 'Tel. Fixe direct'))
